20f79fb473c254da912a534138ce29759176166d
Hazumi Ichijo <gmailii@yahoo.co.jp>
2019-12-02 01:24:53 +0900

    Revert "Rerost/add test function"

--- cmd/wire_gen.go
+++ cmd/wire_gen.go
@@ -10,15 +10,9 @@ import (
 	"context"
 	"github.com/googleapis/google-cloud-go-testing/bigquery/bqiface"
 	"github.com/pkg/errors"
-	"github.com/rerost/bq-table-validator/domain/bqquery"
-	"github.com/rerost/bq-table-validator/domain/tablemock"
-	"github.com/rerost/bq-table-validator/domain/validator"
-	"github.com/rerost/bqv/domain/annotateparser"
 	"github.com/rerost/bqv/domain/viewmanager"
 	"github.com/rerost/bqv/domain/viewservice"
 	"github.com/spf13/cobra"
-	"go.uber.org/zap"
-	"time"
 )
 
 // Injectors from wire.go:
@@ -31,18 +25,7 @@ func InitializeCmd(ctx context.Context, cfg Config) (*cobra.Command, error) {
 	}
 	bqManager := viewmanager.NewBQManager(bqClient)
 	fileManager := NewFileManager(cfg)
-	extractor := annotateparser.NewExtractor()
-	parser := annotateparser.NewParser()
-	manifests := annotateparser.NewManifests(extractor, parser)
-	client, err := NewRawBQClient(ctx, cfg)
-	if err != nil {
-		return nil, err
-	}
-	middleware := NewBQMiddleware(client)
-	time := NewTime()
-	tableMock := tablemock.NewTableMock(time)
-	validatorValidator := validator.NewValidator(middleware, tableMock)
-	command := NewCmdRoot(ctx, viewService, bqManager, fileManager, manifests, validatorValidator)
+	command := NewCmdRoot(ctx, viewService, bqManager, fileManager)
 	return command, nil
 }
 
@@ -58,22 +41,4 @@ func NewBQClient(ctx context.Context, cfg Config) (viewmanager.BQClient, error) 
 
 func NewFileManager(cfg Config) viewmanager.FileManager {
 	return viewmanager.NewFileManager(cfg.Dir)
-}
-
-func NewRawBQClient(ctx context.Context, cfg Config) (bqiface.Client, error) {
-	zap.L().Debug("Create BQ Client", zap.String("ProjectID", cfg.ProjectID))
-	bqClient, err := bigquery.NewClient(ctx, cfg.ProjectID)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	return bqiface.AdaptClient(bqClient), nil
-}
-
-func NewBQMiddleware(bqClient bqiface.Client) validator.Middleware {
-	return bqquery.NewBQQuery(bqClient)
-}
-
-func NewTime() time.Time {
-	return time.Now()
 }
